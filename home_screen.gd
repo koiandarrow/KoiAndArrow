e1dbdf33bf30470171c6a8705c0a1cfb6d2578a4
Rusty <koiandarrowdev@gmail.com>
2024-03-25 09:42:59 -0400

    Added P2P rendezvous

--- home_screen.gd
+++ home_screen.gd
@@ -34,11 +34,78 @@ func show_game_over():
 	#
 func _on_start_button_pressed():
 	$StartButton.hide()
+	$MultiplayerButton.hide()
 	$Message.hide()
 	#$ScoreLabel.show()
 	#$CornCounter.show()
 	#$FishCounter.show()
 	start_game.emit()
+	
+func _on_multiplayer_button_pressed():
+	$StartButton.hide()
+	$Message.hide()
+	$MultiplayerButton.hide()
+	$MultiplayerButton2.hide()
+	start_server()
+	
+func _on_multiplayer2_button_pressed():
+	$StartButton.hide()
+	$Message.hide()
+	$MultiplayerButton.hide()
+	$MultiplayerButton2.hide()
+	start_client()
+	
+func start_server():
+	var hole_puncher = preload('res://addons/Holepunch/holepunch_node.gd').new()
+	# your rendezvous server IP or domain
+	hole_puncher.rendevouz_address = "ip"
+	# the port the HolePuncher python application is running on
+	hole_puncher.rendevouz_port = 443
+	add_child(hole_puncher)
+	# Generate a unique ID for this machine
+	var player_id = OS.get_unique_id()
+	#var player_id = "dummy"
+	var game_code = "12345"
+	var is_host = true
+	var player_host = 'host' if is_host else 'client'
+	var traversal_id = '%s_%s' % [OS.get_unique_id(), player_host]
+	hole_puncher.start_traversal(game_code, true, player_id)
+	# Yield an array of [own_port, host_port, host_ip]
+	# Start a host
+	var result = await hole_puncher.hole_punched
+	var my_port = result[0]
+
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(my_port, 1)
+	get_tree().set_network_peer(peer)
+	start_game.emit()
+
+func start_client():
+	var hole_puncher = preload('res://addons/Holepunch/holepunch_node.gd').new()
+	# your rendezvous server IP or domain
+	hole_puncher.rendevouz_address = "ip"
+	# the port the HolePuncher python application is running on
+	hole_puncher.rendevouz_port = 443
+	add_child(hole_puncher)
+	# Generate a unique ID for this machine
+	var player_id = OS.get_unique_id()
+	#var player_id = "dummy"
+	var game_code = "12345"
+	var is_host = false
+	var player_host = 'host' if is_host else 'client'
+	var traversal_id = '%s_%s' % [OS.get_unique_id(), player_host]
+	hole_puncher.start_traversal(game_code, false, player_id)
+	# Yield an array of [own_port, host_port, host_ip]
+	# Start a host
+	var result = await hole_puncher.hole_punched
+	var host_ip = result[2]
+	var host_port = result[1]
+	var own_port = result[0]
+
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(host_ip, host_port, 0, 0, own_port)
+	get_tree().set_network_peer(peer)
+	
 	
 func _on_dash_pressed():
 	dash.emit()
